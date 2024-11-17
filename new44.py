f59a7d5419bf807fc8a179d97a445136a7f063fe
Sarthak Pandey <www.sarthak025@gmail.com>
2024-11-17 17:29:08 +0530

    Add files via upload

---
+++ new44.py
@@ -0,0 +1,49 @@
+# s="India"
+# for I in s:
+#     print(I,end="")
+
+# s="ilovepython"
+# for ch in range(0,len(s),2):
+#     print(s[ch],end="")
+
+# s="India"
+# i=0
+# while i<len(s):
+#     print(s[i],end="")
+#     i+=1
+
+# s="i love"
+# print(s)
+# s[0]="u"
+# print(s)
+
+# def fx():
+#     s="lovely professional university"
+#     print(s[0:len(s):2])
+# fx()
+
+# s="my name is ishan"
+# x="ishan" in s
+# print(x or False)
+
+# Q. WAP to print all letters from word1 that also appear in word2
+# w1="USA North America"
+# w2="USA South America"
+# i=0
+# for i in range(i,len(w1)):
+#     if w1[i]==w2[i]:
+#         print(w1[i],end="")
+
+# w1="USA North America"
+# w2="USA South America"
+# for letter in w1:
+#     if letter in w2:
+#         print(letter,end="")         #this is true
+
+# s1="aAAA"
+# s2="Azzz"
+# print(s1>s2)   #True
+
+# s1="my name is Ishan"
+# print(s1.split( ))
+
